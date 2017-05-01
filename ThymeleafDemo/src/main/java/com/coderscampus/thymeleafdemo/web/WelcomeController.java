9b5a141e5aaeee857b3f8dc230c9a85af84eda97
Trevor Page <trevor@craftycodr.com>
2017-05-01 10:00:03 -0400

    Part 2

--- ThymeleafDemo/src/main/java/com/coderscampus/thymeleafdemo/web/WelcomeController.java
+++ ThymeleafDemo/src/main/java/com/coderscampus/thymeleafdemo/web/WelcomeController.java
@@ -1,14 +1,30 @@
 package com.coderscampus.thymeleafdemo.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController
 {
   @RequestMapping("/")
-  public String welcome ()
+  public String welcome (ModelMap model)
   {
+    model.put("message", "This message comes to you from our Welcome Controller");
+    
+    List<String> cars = new ArrayList<>();
+    
+    cars.add("Toyota");
+    cars.add("Honda");
+    cars.add("Tesla");
+    cars.add("Ford");
+    cars.add("GM");
+    
+    model.put("cars", cars);
+    
     return "welcome";
   }
 }
